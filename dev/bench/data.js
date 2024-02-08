74b6a7514ce3f8aa01474c4ab1c8a46a843336eb
github-action-benchmark <github@users.noreply.github.com>
2024-02-08 06:20:53 +0000

    add Agents.jl Benchmark (julia) benchmark result for 6c90a9968effec079bf35fa72a20e52201c3371f

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1707373253758,
+  "repoUrl": "https://github.com/InstituteforDiseaseModeling/abm-software-comparison",
+  "entries": {
+    "Agents.jl Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "56368533+krosenfeld-IDM@users.noreply.github.com",
+            "name": "Katherine Rosenfeld",
+            "username": "krosenfeld-IDM"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6c90a9968effec079bf35fa72a20e52201c3371f",
+          "message": "fix tool call",
+          "timestamp": "2024-02-07T22:13:39-08:00",
+          "tree_id": "e53b3f3e9e1c16f4fac1b5127978989e64b43f1c",
+          "url": "https://github.com/InstituteforDiseaseModeling/abm-software-comparison/commit/6c90a9968effec079bf35fa72a20e52201c3371f"
+        },
+        "date": 1707373253389,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "A/4",
+            "value": 251810629,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=2658416\nallocs=57663\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      }
+    ]
+  }
+}
