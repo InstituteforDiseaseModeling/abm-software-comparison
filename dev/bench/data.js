da0fc2c25f676a024cda3e77cedb3c92480866a8
github-action-benchmark <github@users.noreply.github.com>
2024-02-09 01:22:29 +0000

    add Agents.jl Benchmark (julia) benchmark result for 8249da765acc616e7ccfc908e8b448e340276766

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707375888132,
+  "lastUpdate": 1707441749403,
   "repoUrl": "https://github.com/InstituteforDiseaseModeling/abm-software-comparison",
   "entries": {
     "Agents.jl Benchmark": [
@@ -66,6 +66,54 @@ window.BENCHMARK_DATA = {
             "value": 439909042800,
             "unit": "ns",
             "extra": "gctime=11498817\nmemory=98228320\nallocs=2040635\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56368533+krosenfeld-IDM@users.noreply.github.com",
+            "name": "Katherine Rosenfeld",
+            "username": "krosenfeld-IDM"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8249da765acc616e7ccfc908e8b448e340276766",
+          "message": "Update agents.yml \\build",
+          "timestamp": "2024-02-08T16:54:14-08:00",
+          "tree_id": "c9059c60bf462a8a9e52d9f13208edc5dd0608a1",
+          "url": "https://github.com/InstituteforDiseaseModeling/abm-software-comparison/commit/8249da765acc616e7ccfc908e8b448e340276766"
+        },
+        "date": 1707441748986,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "B/4",
+            "value": 323781479.5,
+            "unit": "ns",
+            "extra": "gctime=1309901.5\nmemory=39742224\nallocs=2369763\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "B/6",
+            "value": 52452503296,
+            "unit": "ns",
+            "extra": "gctime=318248229\nmemory=4060664160\nallocs=249659868\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "A/4",
+            "value": 239281413,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=2653312\nallocs=57591\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "A/6",
+            "value": 496525015645,
+            "unit": "ns",
+            "extra": "gctime=12103643\nmemory=95746160\nallocs=2040591\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
