fcc39751659cdc3b3648b91a95fa4716f7508b74
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2024-02-13 21:33:09 +0000

    add Model D

--- examples/Agents.jl/main.jl
+++ examples/Agents.jl/main.jl
@@ -5,20 +5,22 @@ include("ModelB.jl")
 import .ModelB
 include("ModelC.jl")
 import .ModelC
+include("ModelD.jl")
+import .ModelD
+
 
 suite = BenchmarkGroup()
 
 suite["A"] = BenchmarkGroup()
 
 suite["A"][4] = @benchmarkable ModelA.run_model(10^4)
-suite["A"][6] = @benchmarkable ModelA.run_model(10^6)
+# suite["A"][6] = @benchmarkable ModelA.run_model(10^6)
 
 suite["B"] = BenchmarkGroup()
 suite["B"][4] = @benchmarkable ModelB.run_model(10^4)
-suite["B"][6] = @benchmarkable ModelB.run_model(10^6)
+# suite["B"][6] = @benchmarkable ModelB.run_model(10^6)
 
 suite["C"] = BenchmarkGroup()
-suite["C"][6] = @benchmarkable ModelC.run_model(num_agents=10^6, num_days=35*365)
 suite["C"][4] = @benchmarkable ModelC.run_model(num_agents=10^4, num_days=35*365)
 
 tune!(suite)
